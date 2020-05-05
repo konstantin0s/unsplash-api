4de826989eb451793358b4b1fb8da3c55e0cc159
konstantin0s <constantintofan85@gmail.com>
2020-05-05 16:22:08 +0200

    Hit api, create components and install packages

---
+++ client/src/components/Unsplash.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import './css/unsplash.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+// import Sharing from './Sharing';
+import ReactFancyBox from 'react-fancybox';
+import 'react-fancybox/lib/fancybox.css';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 160,
+    width: 345
+  },
+});
+
+ const Unsplash = (props) => {
+    const classes = useStyles();
+
+    const {unsplash } = this.props;
+
+    return (
+        <div className="child-splash">
+      <Card className={classes.root}>
+        <CardActionArea>
+               <ReactFancyBox className={classes.media} thumbnail={unsplash.urls.regular}
+                image={unsplash.urls.regular} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+            {unsplash.user.username}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            {unsplash.description}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            {unsplash.likes} likes
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+           Created: {unsplash.created_at} 
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            {/* <Sharing id={props.gif.id}/> */}
+          </Button>
+       
+          {/* <Link className="linkx" id={props.gif.id} to={`/unsplash/${props.gif.id}`}>
+                  See More..
+        </Link> */}
+        
+        </CardActions>
+      </Card>
+      </div>
+    );
+}
+
+
+export default Unsplash;
--- client/src/components/UnsplashList.js
+++ client/src/components/UnsplashList.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Unsplash from './Unsplash';
+import Loading from './Loading';
+import axios from 'axios';
 
  class UnsplashList extends Component {
 constructor(props) {
@@ -6,14 +9,46 @@ constructor(props) {
 
     this.state = {
         unsplashlist: [],
-        isLoading: false
+        isLoading: true
     }
 }
 
+abracadabra = () => {
+    axios.get('/unsplash')
+    .then(res => {
+        const unsplash = res.data;
+        this.setState({
+            unsplashlist: unsplash,
+            isLoading: false
+        })
+    })
+    .catch(err => console.log(err))
+}
+
+componentDidMount = () => {
+    this.abracadabra();
+}
+
+
     render() {
+    let {unsplashlist, isLoading } = this.state;
+    console.log( unsplashlist);
         return (
-            <div>
-                <p>unsplashlist</p>
+
+            <div className="unsplash-list">
+                <div>
+                                        {
+
+                                            
+                        (isLoading) ? <Loading /> : 
+
+
+                        unsplashlist.map((unsplash) => (
+
+                                <Unsplash key={unsplash.id} unsplash={unsplash} />
+                            ))   } 
+
+                </div>
             </div>
         )
     }
