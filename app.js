2c8e03d54cccc2eb9e50f34b2d337da7f10eb5f2
konstantin0s <constantintofan85@gmail.com>
2020-05-10 17:16:52 +0200

    prepare to deploy

--- app.js
+++ app.js
@@ -40,11 +40,11 @@ app.use(function(req, res, next) {
 
 
           // Set static folder
-          // app.use(express.static('client/build'));
+          app.use(express.static('client/build'));
       
-          // app.get('*', (req, res) => {
-          //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-          // });
+          app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+          });
         
         
 
